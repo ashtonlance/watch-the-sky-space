3315e497e9d73456b3df05a15a524d48e81c4780
Ashton Lance <ashton@ashtonlance.com>
2017-04-14 08:47:40 -0400

    Some package updates
    User’s location now persists over different vists via localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    
+  
     <section id="wrapper"
              class="hero is-primary">
-             <vue-progress-bar></vue-progress-bar>
+      <vue-progress-bar></vue-progress-bar>
       <div id="moonwrapper">
         <h4 class="title is-4">star-gazing forecast</h4> {{ moon }}
       </div>
       <div style="padding: 2.5rem 0.5rem;"
            class="hero-body">
-        <h2 class="title is-4">{{ phase }} :: {{ illum }}% full</h2>
-        <p>next full moon :: {{ next }} days</p>
+  
+        <h2 class="title is-4 is-spaced">{{ phase }} :: {{ illum }}% full</h2>
+        <p class="subtitle is-spaced">sunset in {{ sunset }} hours</p>
+        <p class="subtitle">next full moon :: {{ next }} days</p>
       </div>
   
-      <div id="weatherwrapper">
-        <h1 class="title"> {{ weather }} </h1>
-        <h2 class="subtitle">weather for {{ city }} </h2>
-        <input style="margin:10px auto;"
-               class="label"
-               type="text"
-               v-model="zip"
-               placeholder="Enter Zip Code">
-        <button class="button is-dark"
-                v-on:click="getWeatherDynamic()">enter</button>
+      <div id="weatherwrapper" class="is-spaced">
+        <h1 class="title is-marginless"> {{ weather }} </h1>
+        <h2 class="subtitle is-marginless">tonight's forecast for {{ city }} </h2>
+  
+        <b-field position="centered">
+          <b-input class="label"
+                   type="text"
+                   v-model="zip"
+                   placeholder="Enter Zip Code"></b-input>
+          <p class="control">
+            <button class="button is-dark"
+                    v-on:click="getWeatherDynamic()">enter</button>
+          </p>
+        </b-field>
       </div>
     </section>
   </div>
@@ -41,36 +47,47 @@ export default {
       next: '',
       moon: '',
       zip: '37408',
-      weather: ''
+      weather: '',
+      sunset: ''
     }
   },
 
   methods: {
+    
     getPhase() {
       this.$http.get('http://api.aerisapi.com/sunmoon/37408?client_id=jYskRupqMPlhogr2iY4i3&client_secret=pvhD0Ydih22gZXcIBDbpMzPlXdzVziJ0pyeRav3Y').then((response) => {
         this.phase = response.data.response[0].moon.phase.name;
         this.illum = response.data.response[0].moon.phase.illum;
         this.city = response.data.response[0].place.name;
         this.moon = (moonmoji.emoji);
-        this.$Progress.finish()
+
       })
     },
     getWeatherDynamic() {
       this.$Progress.start()
-      this.$http.get('http://api.aerisapi.com/forecasts/' + this.zip + '?from=today&to=today&client_id=jYskRupqMPlhogr2iY4i3&client_secret=pvhD0Ydih22gZXcIBDbpMzPlXdzVziJ0pyeRav3Y').then((response) => {
+      let startOfNight = this.$moment().format('YYYY-MM-DD 20:00')
+      let now = this.$moment(startOfNight).format()
+      // now = this.$moment(now).format('YYYY-MM-DD HH:mm ')
+      let tonight = this.$moment(now).add(12, 'h').format('YYYY-MM-DD HH:mm')
+      this.zip = localStorage.getItem('zip'),
+      
+      this.$http.get('http://api.aerisapi.com/forecasts/' + this.zip + '?from=' + now + '&to=' + tonight + '&client_id=jYskRupqMPlhogr2iY4i3&client_secret=pvhD0Ydih22gZXcIBDbpMzPlXdzVziJ0pyeRav3Y').then((response) => {
         this.weather = response.data.response[0].periods[0].weatherPrimary.toLowerCase();
       }),
         this.$http.get('http://api.aerisapi.com/sunmoon/' + this.zip + '?client_id=jYskRupqMPlhogr2iY4i3&client_secret=pvhD0Ydih22gZXcIBDbpMzPlXdzVziJ0pyeRav3Y').then((response) => {
           this.city = response.data.response[0].place.name;
-
+          let sunsetRaw = (response.data.response[0].sun.setISO);
+          let today = this.$moment().format();
+          this.sunset = this.$moment(sunsetRaw).diff(today, 'hours');
         })
-        this.$Progress.finish()
+      this.$Progress.finish()
     },
     getNextFull() {
       this.$http.get('http://api.aerisapi.com/sunmoon/moonphases/chattanooga,tn&search?query=type:full&limit=1&client_id=jYskRupqMPlhogr2iY4i3&client_secret=pvhD0Ydih22gZXcIBDbpMzPlXdzVziJ0pyeRav3Y').then((response) => {
         let nextRaw = (response.data.response[0].dateTimeISO);
         let today = this.$moment().format();
         this.next = this.$moment(nextRaw).diff(today, 'days');
+        this.$Progress.finish()
       })
     }
   },
@@ -78,16 +95,37 @@ export default {
     this.getPhase();
     this.getNextFull();
     this.getWeatherDynamic();
-    
+
   },
-  created () {
-    this.$Progress.start()    
-  }
+  created() {
+    this.$Progress.start()
+  },
+  watch: {
+    zip() { 
+        localStorage.setItem('zip', this.zip)    
+    }
+}
 }
 </script>
 
 <style lang="scss">
+html {
+  height: 100%;
+}
+
+body {
+  position: absolute;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
 #app {
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -97,6 +135,7 @@ export default {
 
 
   #wrapper {
+    height: 100%;
     background: rgba(#000, 0.35);
     padding: 25px;
     padding-top: 5px;
@@ -133,9 +172,9 @@ export default {
   }
 
   #weatherwrapper {
-
     border-radius: 4px;
     padding: 5px;
+    margin-bottom: 15%;
   }
 }
 </style>
